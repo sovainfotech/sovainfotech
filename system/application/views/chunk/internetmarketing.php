89d1eafcc1dcf08d4b3e13893c6b4a3e463faaf0
washim <washim.ahmed@gmail.com>
2011-05-25 16:03:46 +0530

    internet marketting change

--- system/application/views/chunk/internetmarketing.php
+++ system/application/views/chunk/internetmarketing.php
@@ -1,11 +1,20 @@
+<script>
+$(function() {
+	$('a[id*="mybutton"]').button({
+		icons:{
+        	primary: "ui-icon-gear"
+        }
+	});
+});
+</script>
 <h1>Internet Marketing</h1>
 <h3>Reach Your Customer Online & Multiply Sales Infinitely</h3><br />
 <h2>Internet Marketing Services, Your Business Website Promotion & Sova Infotech</h2><br />
 <img src="<?=base_url()?>media/images/e-business.jpg" class="image-float" alt="internet marketing" title="internet marketing">
-<p>The online market is now burgeoning worldwide like never before. World has come to the verge of an online revolution. Thus, online presence and website promotion has become a prerequisite for a business to enhance its sales. More and more businesses – large, medium, small and start-ups – are realising the importance of their online presence. Hence, they are now not only developing and designing attractive and professional websites for their businesses but also insisting on an enhanced online presence for establishing their brand and increasing their ROI. Internet marketing can be done through various channels such as search engine optimisation (SEO), paid placements, PPC campaign, social media optimisation, email marketing, affiliate marketing, search engine marketing (SEM) and many more.</p><br />
+<p align="justify">The online market is now burgeoning worldwide like never before. World has come to the verge of an online revolution. Thus, online presence and website promotion has become a prerequisite for a business to enhance its sales. More and more businesses – large, medium, small and start-ups – are realising the importance of their online presence. Hence, they are now not only developing and designing attractive and professional websites for their businesses but also insisting on an enhanced online presence for establishing their brand and increasing their ROI. Internet marketing can be done through various channels such as search engine optimisation (SEO), paid placements, PPC campaign, social media optimisation, email marketing, affiliate marketing, search engine marketing (SEM) and many more.</p><br />
 <h2>What Internet Marketing Services Do Sova Infotech Provide?</h2><br />
 <h4>The internet marketing services provided by Sova Infotech include:</h4>
-<ul style="margin-left:50px;">
+<ul style="margin-left:50px; margin-bottom:15px;">
   <li>Search Engine Marketing (SEM) &nbsp;&nbsp;</li>
   <li>Search Engine Optimisation  (SEO)</li>
   <li>PPC Campaign</li>
@@ -21,5 +30,5 @@
   <li>Improve conversion rate</li>
   <li>Help businesses of all sizes  and shapes to increase their revenues as well as profits</li>
 </ul><br />
-<p>Our internet marketing experts and specialists can bring significant improvement in your existing internet marketing campaigns or design a comprehensive and structured online marketing strategy from scratch. The complete transparent approach, dedicated care of handling each client's project, continuous client communication and cost effectiveness places us ahead of our peers. We are regarded as one of the leading internet marketing consultants. If you are looking for internet marketing solution for your business then please avail the comprehensive online marketing services of Sova Infotech and feel the difference.</p><br />
-<h4>Internet Marketing, Internet Marketing Company India, Internet Marketing specialist, Internet Marketing expert, website promotion, internet marketing services, Sova Infotech, best Internet Marketing, Internet Marketing consultants, seo, online internet marketing, research, internet marketing for online business, search engine optimisation, social media optimisation, search engine marketing</h4>
+<p align="justify">Our internet marketing experts and specialists can bring significant improvement in your existing internet marketing campaigns or design a comprehensive and structured online marketing strategy from scratch. The complete transparent approach, dedicated care of handling each client's project, continuous client communication and cost effectiveness places us ahead of our peers. We are regarded as one of the leading internet marketing consultants. If you are looking for internet marketing solution for your business then please avail the comprehensive online marketing services of Sova Infotech and feel the difference.</p><br />
+<div align="right"><a href="<?=site_url('sova/quote')?>" id="mybutton">Get Quote</a></div>
